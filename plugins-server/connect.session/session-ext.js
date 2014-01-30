2c8ade688f4ca98f00e70d35581a0042d6e065c8
srossross <srossross@gmail.com>
2014-01-30 10:51:43 -0800

    ENH: updated session cookie to limit to url-prefix

--- plugins-server/connect.session/session-ext.js
+++ plugins-server/connect.session/session-ext.js
@@ -9,10 +9,14 @@ module.exports = function startup(options, imports, register) {
     var connect = imports.connect;
     var sessionStore = imports["session-store"];
 
+    var argv = require('optimist').argv;
+    var urlPrefix = argv['url-prefix'];
+
     connect.useSession(Session({
         store: sessionStore,
         key: options.key,
-        secret: options.secret
+        secret: options.secret,
+        cookie: { path: urlPrefix || '/', httpOnly: true, maxAge: null }
     }));
 
     register(null, {
